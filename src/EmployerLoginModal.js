e63ee5b8dd8a49a283fdd22b9a497aa831f6ba27
qwetuo <chewminzhuang@hotmail.com>
2018-11-14 14:33:24 +0800

    connected employer login to api route /signin/employer (yet to redirect)

--- src/EmployerLoginModal.js
+++ src/EmployerLoginModal.js
@@ -1,8 +1,56 @@
 import React, { Component } from "react";
-import { Button, Modal } from "semantic-ui-react";
+import { Button, Modal, Message } from "semantic-ui-react";
 import EmployerSignUpPage from "./EmployerSignUpPage";
+import { API_URL } from "./utils/configVar";
 
 class EmployerLoginModal extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      err: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    const response = await fetch(`${API_URL}/signin/employer`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      this.setState({
+        err: "Sorry, this feature is not ready yet. Do check out the prototype account instead"
+      })
+      // this.props.history.push({
+      //   pathname: `employer/${this.state.username}`
+      // });
+    } else {
+      this.setState({
+        err: data.message
+      });
+    }
+  };
+
   handleRedirect = () => {
     this.props.history.push({
       pathname: "/employer/prototype"
@@ -16,42 +64,46 @@ class EmployerLoginModal extends Component {
           <Modal.Header>Employer Login</Modal.Header>
           <Modal.Content image scrolling>
             <Modal.Description>
-              <div class="ui middle aligned center aligned grid">
-                <div class="column">
-                  <form method="get" class="ui large form">
-                    <div class="ui stacked secondary  segment">
-                      <div class="field">
-                        <div class="ui left icon input">
-                          <i class="user icon" />
+              <div className="ui middle aligned center aligned grid">
+                <div className="column">
+                  <form method="get" className="ui large form">
+                    <div className="ui stacked secondary  segment">
+                      <div className="field">
+                        <div className="ui left icon input">
+                          <i className="user icon" />
                           <input
                             type="text"
-                            name="email"
-                            placeholder="E-mail address"
+                            name="username"
+                            value={this.state.username}
+                            onChange={this.handleChange}
+                            placeholder="Username"
                           />
                         </div>
                       </div>
-                      <div class="field">
-                        <div class="ui left icon input">
-                          <i class="lock icon" />
+                      <div className="field">
+                        <div className="ui left icon input">
+                          <i className="lock icon" />
                           <input
                             type="password"
                             name="password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
                             placeholder="Password"
                           />
                         </div>
                       </div>
-                      <Button
-                        fluid
-                        type="submit"
-                        onClick={() =>
-                          this.props.loadComponent("EmployerHomePage")
-                        }
-                      >
+                      <Button fluid type="submit" onClick={this.handleSubmit}>
                         Log In
                       </Button>
                     </div>
 
-                    <div class="ui error message" />
+                    {this.state.err === "" ? null : (
+                      <Message negative>
+                        <Message.Header>Error:</Message.Header>
+                        <p>{this.state.err}</p>
+                      </Message>
+                    )}
+
                   </form>
 
                   <Button primary onClick={this.handleRedirect}>
