8837a0fe03b9f9e7a54fd3a7cb1081666987109d
qwetuo <chewminzhuang@hotmail.com>
2018-12-06 23:11:24 +0800

    amended seeded data to get dates in relation to 'today'

--- src/seedData.js
+++ src/seedData.js
@@ -1,3 +1,18 @@
+import addDays from 'date-fns/add_days'
+
+const today = new Date()
+
+const getFormattedDate = (date) => {
+  let dd = date.getDate().toString();
+  dd = dd.length === 2 ? dd : "0" + dd
+  let mm = (date.getMonth() + 1).toString()
+  mm = mm.length === 2 ? mm : "0" + mm
+  const yyyy = date.getFullYear().toString();
+  return yyyy + mm + dd
+}
+
+const next10DaysDates = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(dayLater => getFormattedDate(addDays(today,dayLater)))
+
 const postings1 = [
   {
     title: "Flyer Distribution",
@@ -8,18 +23,7 @@ const postings1 = [
         * Salary only paid after assignment is done`,
     location: "Bedok North",
     type: "others",
-    commitment: [
-      "20180630",
-      "20180701",
-      "20180707",
-      "20180714",
-      "20180721",
-      "20180708",
-      "20180722",
-      "20180728",
-      "20180715",
-      "20180729"
-    ],
+    commitment: next10DaysDates.slice(0,2),
     status: "active",
     interested: ["int1", "int2", "int3"],
     applicants: ["app1", "app2"],
@@ -32,7 +36,7 @@ const postings1 = [
     req: `posting2test1`,
     location: "posting2test1",
     type: "others",
-    commitment: ["20180630", "20180714", "20180715", "20180716", "20180717"],
+    commitment: next10DaysDates.slice(0,1),
     status: "active",
     interested: ["int3", "int4"],
     applicants: ["app3"],
@@ -44,14 +48,7 @@ const postings1 = [
     desc: "posting3test1",
     req: `posting3test1`,
     type: "others",
-    commitment: [
-      "20180630",
-      "20180727",
-      "20180720",
-      "20180803",
-      "20180810",
-      "20180817"
-    ],
+    commitment: next10DaysDates.slice(2,5),
     status: "active",
     location: "posting3test1",
     interested: ["int5", "int6"],
@@ -67,7 +64,7 @@ const postings2 = [
     desc: "desc 2.1",
     req: `req 2.1`,
     type: "others",
-    commitment: ["20180701", "20180702"],
+    commitment: next10DaysDates.slice(3,9),
     status: "active",
     location: "location 2.1",
     interested: ["int"],
@@ -80,7 +77,7 @@ const postings2 = [
     desc: "desc 2.2",
     req: `req 2.2`,
     type: "others",
-    commitment: ["20180630", "20180714"],
+    commitment: next10DaysDates,
     status: "active",
     location: "location 2.2",
     interested: [],
@@ -93,7 +90,7 @@ const postings2 = [
     desc: "desc 2.3",
     req: `req 2.3`,
     type: "others",
-    commitment: ["20180630"],
+    commitment: next10DaysDates.slice(1,5),
     status: "active",
     location: "location 2.3",
     interested: [],
